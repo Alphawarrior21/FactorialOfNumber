1509834fc46afb8c027cd0ff8318d09ef6028b58
sumit chand <sumitchandmale@gmail.com>
2023-01-14 22:59:25 +0530

    Program to Calculate Factorial of a number C#

---
+++ FactorialOfNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FactorialOfNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number to calculate the factorial of a number");
+            int i= Convert.ToInt32(Console.ReadLine());
+            int fact=GetFactorial(i);
+            Console.WriteLine($"The factorial of {i} is {fact}");
+
+        }
+
+        private static int GetFactorial(int n)
+        {
+            if (n == 1 || n == 0)
+                return 1;
+            else
+                return n * GetFactorial(n - 1);
+           
+        }
+    }
+}
